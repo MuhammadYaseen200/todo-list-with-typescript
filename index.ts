db594210a7db116de6bb77357a0b176a537a9e62
MuhammadYaseen200 <muhammadyaseenabdulghanijumani@gmail.com>
2024-04-14 21:35:29 +0500

    todo list with js

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition= true;
+while(condition){
+    let addTask = await inquirer.prompt(
+    [
+        {
+            name:"todo",
+            message:"What you want add in you Todos?",
+            type:"input",
+
+        },
+
+        {
+            name:"addMore",
+            message:"Do you want to add more in your todos",
+            type: "confirm",
+            default:"false",
+        }
+    ]
+);
+todos.push(addTask.todo);
+condition = addTask.addMore
+console.log(todos);
+}
